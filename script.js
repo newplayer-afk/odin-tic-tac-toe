b5e064dd40be2278edc8371b70817360d0e110cc
newplayer-afk <sdc.startup@gmail.com>
2024-09-04 19:37:07 -0400

    create gameBoard IIFE

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+//PLAYER FACTORY FUNCTION
 function player(name) {
     return function marker(choice){
         return {name, choice}
     }
 }
 
-//PLAYERS
+//CREATE PLAYERS
 const sean = player('Sean')('X');
 const jackie = player('Jackie')('O');
 
 //PLAYER CHECK
 console.log(sean)
 console.log(jackie)
+
+//GAMEBOARD FACTORY FUNCTION
+const gameBoard = (function() {
+    const board = [[],[],[]]
+
+    return {board}
+})
+
+console.log(gameBoard())
